3bc36f28c699a009253b77923907b0be6e7b6a51
hritu567 <hritumog96@gmail.com>
2020-11-26 20:02:00 +0530

    Added : pattern for valid email

--- utility.js
+++ utility.js
@@ -1,5 +1,5 @@
-//UC2
-//checking valid last name
+//UC3
+//checking valid email
 function userRegistration()
 {
    var readline = require('readline');
@@ -8,12 +8,12 @@ function userRegistration()
      input: process.stdin,
      output: process.stdout
    });
-   rl.question("Enter the last name : " , function(lastName){
-      const pet=/[A-Z]{1}[a-z]{2,}/;
-      if(!pet.test(lastName))
+   rl.question("Enter the email : " , function(email){
+      const pet=/[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-z]{2,4}([.][a-z]{2})*/;
+      if(!pet.test(email))
       console.log("Invalid");
       else
-      console.log("Valid last Name");
+      console.log("Valid email");
       rl.close();
    }
    );
