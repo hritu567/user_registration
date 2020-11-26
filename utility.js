e0021caf73dec3d9d6d8d37c31ca30f419628c54
hritu567 <hritumog96@gmail.com>
2020-11-26 19:53:14 +0530

    Added : pattern for vaild last name

--- utility.js
+++ utility.js
@@ -1,5 +1,5 @@
-//UC1
-//checking valid first name
+//UC2
+//checking valid last name
 function userRegistration()
 {
    var readline = require('readline');
@@ -8,12 +8,12 @@ function userRegistration()
      input: process.stdin,
      output: process.stdout
    });
-   rl.question("Enter the first name : " , function(firstName){
+   rl.question("Enter the last name : " , function(lastName){
       const pet=/[A-Z]{1}[a-z]{2,}/;
-      if(!pet.test(firstName))
+      if(!pet.test(lastName))
       console.log("Invalid");
       else
-      console.log("Valid First Name");
+      console.log("Valid last Name");
       rl.close();
    }
    );
