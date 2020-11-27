e47f031b4c6c91d7dff93b8b65071fe612815b45
hritu567 <hritumog96@gmail.com>
2020-11-27 19:01:47 +0530

    Added : combination of all use cases

--- userregistration.js
+++ userregistration.js
@@ -1,3 +1,12 @@
 console.log("Welcome to User registration program");
-var Utility= require('./utility');
-Utility.userRegistration();
+const Utility= require('./utility');
+var user= new Utility.userRegistration();
+user.firstNamePattern();
+user.lastNameePattern();
+user.validEmail();
+user.passordPattern();
+user.mobilePattern();
+user.passordPatternUpperCase();
+user.passwordPatternNumericNum();
+user.passwordPatternSpeChar();
+
--- utility.js
+++ utility.js
@@ -1,21 +1,79 @@
-//UC8
-//password rule-has exactly 1 special character 
-function userRegistration()
-{
-   var readline = require('readline');
-
-   var rl = readline.createInterface({
-     input: process.stdin,
-     output: process.stdout
-   });
-   rl.question("Enter the password : " , function(password){
-      const pet=/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$#%*+]).{8,}/;
+//UC9
+//cpmbining all use cases
+var readlinesync=require('readline-sync');
+class userRegistration
+{  //UC1
+   firstNamePattern=()=>{
+      const pet=/[A-Z]{1}[a-z]{2,}/;
+      var firstName=readlinesync.question("Enter the first name: ");
+      if(!pet.test(firstName))
+      console.log("Invalid");
+      else
+      console.log("Valid First Name");
+   } 
+   //UC2
+   lastNameePattern=()=>{
+      const pet=/[A-Z]{1}[a-z]{2,}/;
+      var lastName=readlinesync.question("Enter the last name: ");
+      if(!pet.test(lastName))
+      console.log("Invalid");
+      else
+      console.log("Valid last Name");
+   }
+   //UC3
+   validEmail=()=>{
+      const pet=/[a-zA-Z0-9]+([.+-_][a-zA-Z0-9]+)*@[a-zA-Z0-9]+.[a-z]{2,4}([.][a-z]{2})*/;
+      var email=readlinesync.question("Enter the email: ");
+      if(!pet.test(email))
+      console.log("Invalid");
+      else
+      console.log("Valid email");
+   }
+   //UC4
+   mobilePattern=()=>{
+      const pet=/((\+){1}91){1}[ ]*[0-9]{10}/;
+      var number=readlinesync.question("Enter the number: ");
+      if(!pet.test(number))
+      console.log("Invalid number");
+      else
+      console.log("Valid number");
+   }
+   //UC5
+   passordPattern=()=>{
+      const pet=/[a-zA-Z0-9]{8,}/;
+      var password=readlinesync.question("Enter the password pattern1: ");
       if(!pet.test(password))
       console.log("Invalid password");
       else
       console.log("Valid passord");
-      rl.close();
    }
-   );
+   //UC6
+   passordPatternUpperCase=()=>{
+      const pet=/([a-zA-Z0-9]*[A-Z]+[a-zA-Z0-9]*)/;
+      var password=readlinesync.question("Enter the password pattern2: ");
+      if(!pet.test(password))
+      console.log("Invalid password");
+      else
+      console.log("Valid passord");
+   }
+   //UC7
+   passwordPatternNumericNum=()=>{
+      const pet=/(?=.*[A-Z])(?=.*[0-9])[a-zA-Z0-9]{8,}/;
+      var password=readlinesync.question("Enter the password pattern3: ");
+      if(!pet.test(password))
+      console.log("Invalid password");
+      else
+      console.log("Valid passord");
+   }
+   //UC8
+   passwordPatternSpeChar=()=>
+   {
+      const pet=/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@$#%*+]).{8,}/;
+      var password=readlinesync.question("Enter the password pattern4: ");
+      if(!pet.test(password))
+      console.log("Invalid password");
+      else
+      console.log("Valid passord");
+   }
 }
 module.exports={userRegistration};
